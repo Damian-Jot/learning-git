cc6875293357d6397f02a0f568cc271a5d993c8c
damian.jojczykk <damian.jojczykk@gmail.com>
2025-06-03 15:49:54 +0200

    Poprawka wersji 2.

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
 print("Lista zakupow. Version 2")
+lista_zakupow = {
+    "warzywniak": ["marchew", "seler", "rukola"],
+    "piekarnia": ["chleb", "bułki", "pączki"]
+}
+print("lista_zakupow:")
+for sklep, produkty in lista_zakupow.items():
+    print(f"Ide do {sklep}, kupie: {produkty}") 
+print("W sumie kupie " + str(len(lista_zakupow["warzywniak"]) + len(lista_zakupow["piekarnia"])) + " produktów.")
